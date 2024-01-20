ee1dac9909e2c82efe518348507804fa57a4c7ff
pawel200020 <pawel200020@gmail.com>
2024-01-20 23:07:02 +0100

    refactor

--- Projekt2/project2.pl
+++ Projekt2/project2.pl
@@ -9,9 +9,7 @@ canvasRow(W,R,ARR):- W1 is W-1,canvasRow(W1,R,[' '|ARR]).
 point([],_,_,[]).
 point([H|S1],[X,0],Z,[H2|S2]):- Y1 is -1, replaceRow(X,Z,H,H2), point(S1, [X, Y1], Z, S2). 
 point([H|S1],[X,Y],Z,[H|S2]):- Y1 is Y-1, point(S1, [X, Y1], Z, S2),!. 
-%X - kolumny
-%Y - wiersz
-%liczymy od 0
+
 replaceRow(_, _, [], []).
 replaceRow(0, R, [_|T], [R|T2]) :- replaceRow(-1, R, T, T2).
 replaceRow(Y, R, [H|T], [H|T2]) :- Y1 is Y-1, replaceRow(Y1, R, T, T2),!.
@@ -20,7 +18,6 @@ writeHorizontalLine(_,_, _, [], []).
 writeHorizontalLine(I,0,R, [_|T], [R|T2]) :- I2 is I-1, writeHorizontalLine(I2,-1, R, T, T2).
 writeHorizontalLine(I,Y,R, [_|T], [R|T2]) :- I >= 0, Y<0, I2 is I-1,Y2 is Y-1, writeHorizontalLine(I2,Y2, R, T, T2).
 writeHorizontalLine(I, Y, R, [H|T], [H|T2]) :- Y1 is Y-1, writeHorizontalLine(I,Y1, R, T, T2),!.
-
 
 line(S1,[X1,Y1],[X2,Y2],Z,S2):- line(S1,[X1,Y1],[X2,Y2],Z,0,S2),!.
 line([],[_,_],[_,_],_,_,[]).
@@ -35,12 +32,10 @@ line([H|S1],[X2,Y2],[X1,Yc],Z,Yc,[H2|S2]):- X1-X2 < 0, replaceRow(X1,Z,H,H2), Xn
 line([H|S1],[X2,Y2],[X1,Yc],Z,Yc,[H2|S2]):- X1-X2 > 0, replaceRow(X1,Z,H,H2), Xn is X1-1, Yc1 is Yc+1, line(S1,[Xn,Yc1],[X2,Y2],Z,Yc1,S2).
 line([H|S1],[X1,Y1],[X2,Y2],Z,Yc,[H|S2]):- Yc2 is Yc+1, line(S1,[X1,Y1],[X2,Y2],Z,Yc2,S2).
 
-
 poly(S1,[[X,Y]|R],Z,S2):-poly(S1,[[X,Y]|R],Z,S2,[X,Y]),!.
 poly(_,[],_,_,_):-!.
 poly(S1,[[X,Y]],Z,S3,[X1,Y1]):-poly(S1,[],Z,S3,[]),line(S1,[X,Y],[X1,Y1],Z,S3).
 poly(S1,[[X1,Y1],[X2,Y2]|R],Z,S3,[X0,Y0]):-poly(S1,[[X2,Y2]|R],Z,S2,[X0,Y0]),line(S2,[X1,Y1],[X2,Y2],Z,S3).
-
 
 clean(S1,X1,Y1,X2,Y2,S2):-clean(S1,X1,Y1,X2,Y2,0,S2),!.
 clean([],_,_,_,_,_,[]).
